47ec13f3192b6c0d6e9a015f1c9e3cb8c2442a7d
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-04-25 13:47:10 +0200

    It searches! It does something!

--- Google Chrome Extension1/Google Chrome Extension1/app/popup.js
+++ Google Chrome Extension1/Google Chrome Extension1/app/popup.js
@@ -1,1 +1,30 @@
-//Koden ska in här____
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+function onYouTubeApiLoad() {
+
+    gapi.client.setApiKey('AIzaSyCxZBGJHV6dTszSVQ2c6lKDUmlx5EfmOws');
+
+    search();
+}
+
+function search() {
+
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: "shut up and dance", //Vad den söker på , hårdkodat atm
+    });
+
+    request.execute(onSearchResponse);
+}
+
+
+function onSearchResponse(response) {
+    showResponse(response);
+}
