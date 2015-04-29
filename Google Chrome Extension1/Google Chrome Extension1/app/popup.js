2feb8d83ea78942da891f5274ccf953a8de40fca
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-04-29 13:49:40 +0200

    Pushing is bugged and wierd. will take a look someday

--- Google Chrome Extension1/Google Chrome Extension1/app/popup.js
+++ Google Chrome Extension1/Google Chrome Extension1/app/popup.js
@@ -1,39 +1,45 @@
-function showResponse(response) {
+function showResponse(response) { //Skriver ut svaret på vad som har hämtats från data-Api:et
     var responseString = JSON.stringify(response, '', 2);
     document.getElementById('response').innerHTML += responseString;
 }
 
-function onClientLoad() {
+function onClientLoad() { //När plug:inet laddas körs detta och "onYoutubeApiLoad körs"
     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
 
-function onYouTubeApiLoad() {
+function onYouTubeApiLoad() { //Sätter api nyckeln
 
     gapi.client.setApiKey('AIzaSyCxZBGJHV6dTszSVQ2c6lKDUmlx5EfmOws');
-
-    addSearch();
 }
-var newestsearch ="";
 
-function addSearch() {
+window.addEventListener("load", function () { //Sätter funktion på knappen som finns så att man kan kan använda den till att söka på saker.
+    document.getElementById("searchbutton")
+            .addEventListener("click", addSearch, false);
+}, false);
 
-    var Searchinformation = document.getElementById('searchinput');
-    Searchinformation.textContent = newestsearch;
-    
+var searchinformation = ""; //Variabeln som används för att söka skapas
+String (searchinformation); //Sätts till sträng
+
+function addSearch() { //Hämtar sökrutan, tar informationen och skickar iväg den som sträng, rensar också listan från tidigare sök
+
+    searchinformation = document.getElementById('searchbar').value;
+
+    document.getElementById('response').innerHTML = "";
+
     search();
 }
 
-function search() {
+function search() { //Söker i data-api:et efter relevanta resultat och skriver sedan ut dom via "onSearchResponse"
 
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
-        q: newestsearch,
+        q: searchinformation,
     });
 
     request.execute(onSearchResponse);
 }
 
 
-function onSearchResponse(response) {
+function onSearchResponse(response) { //Startar showResponse!"
     showResponse(response);
 }
