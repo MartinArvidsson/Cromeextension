03d7bff8e8473910469b9f9209ddb9281f83c6bd
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-05-03 23:56:19 +0200

    Worked, moved stuff, added stuff, some works, some don't. still somewhat where my deadline is supposed to be. (ish)

--- Google Chrome Extension1/Google Chrome Extension1/app/popup.js
+++ Google Chrome Extension1/Google Chrome Extension1/app/popup.js
@@ -1,8 +1,3 @@
-function showResponse(response) { //Skriver ut svaret på vad som har hämtats från data-Api:et
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('response').innerHTML += responseString;
-}
-
 function onClientLoad() { //När plug:inet laddas körs detta och "onYoutubeApiLoad körs"
     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
@@ -25,6 +20,7 @@ function addSearch() { //Hämtar sökrutan, tar informationen och skickar iväg den
     searchinformation = document.getElementById('searchbar').value;
 
     document.getElementById('response').innerHTML = "";
+    document.getElementById('Renderlinks').innerHTML = "";
 
     search();
 }
@@ -34,6 +30,8 @@ function search() { //Söker i data-api:et efter relevanta resultat och skriver s
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
         q: searchinformation,
+        maxResults: 10,
+        type: 'video',
     });
 
     request.execute(onSearchResponse);
@@ -42,4 +40,33 @@ function search() { //Söker i data-api:et efter relevanta resultat och skriver s
 
 function onSearchResponse(response) { //Startar showResponse!"
     showResponse(response);
+}
+
+function showResponse(response) { //Skriver ut svaret på vad som har hämtats från data-Api:et
+    console.log(response)
+    //var responseString = JSON.stringify(response, '', 2);
+    //document.getElementById('response').innerHTML += responseString;
+
+    for (var i = 0; i < response.items.length; i++) { //<-- fixar tumnagelbilder , fungerar inte. Bilderna syns inte, visas dock i konsolen.
+        var title = document.createElement("h1");
+        var atag = document.createElement("a");
+        atag.className = "Thumbnail";
+        atag.href = "#";
+        atag.image = response.items[i].snippet.thumbnails.default.url;
+        atag.style.width = 120 + "px";
+        atag.style.height = 90 + "px";
+
+        atag.onclick = function () {
+            var video = response.items[i].id.videoID;
+            //Skicka med video variablen till en videospelare, Nästa veckas jobb.
+        };
+        console.log(atag.image);
+
+        var Title = response.items[i].snippet.title;
+        title.innerHTML = Title;
+        console.log(Title);
+
+        Renderlinks.appendChild(title);
+        Renderlinks.appendChild(atag);        
+    }
 }
